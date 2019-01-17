697b3283ffe21bdadd413de19867c9d407e25a32
Itai Gendler <itai@codefresh.io>
2019-01-17 08:10:40 +0200

    update git gc to use force flag to still execute even if there is another gc process running on the repository

--- start.sh
+++ start.sh
@@ -61,7 +61,7 @@ if [ -d "$CLONE_DIR" ]; then
       echo "Cleaning up the working directory"
       git reset -q --hard
       git clean -df
-      git gc
+      git gc --force
       git_retry git remote prune origin
 
       echo "Fetching the updates from origin"
