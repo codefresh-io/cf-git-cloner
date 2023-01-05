bb5b156bcefbb5a6274505e9356f2b11f7b1c899
eti-codefresh <eti.zaguri@codefresh.io>
2023-01-05 09:52:16 +0200

    WIP

--- start.sh
+++ start.sh
@@ -99,8 +99,10 @@ if [ -n "$SPARE_CHECKOUT" ]; then
  fi
 
 if [ -n "$DEPTH" ]; then
+  echo "Using DEPTH $DEPTH"
   GIT_COMMAND="git_retry git clone $REPO $CLONE_DIR --depth=$DEPTH"
 else
+  echo "no DEPTH"
   GIT_COMMAND="git_retry git clone $REPO $CLONE_DIR"
 fi
 
