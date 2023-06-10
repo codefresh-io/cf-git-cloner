43c5eb629da365e09ee36872c587cb53f97ab335
eti-codefresh <eti.zaguri@codefresh.io>
2023-06-10 04:11:28 +0300

    remove logs

--- start.sh
+++ start.sh
@@ -194,15 +194,11 @@ if [ -d "$CLONE_DIR" ]; then
       rm -rf $CLONE_DIR
       eval $GIT_COMMAND
       cd $CLONE_DIR
-      echo "The folder already exists but it is not a git repository - cloned successfully into $CLONE_DIR"
+
       if [ -n "$REVISION" ]; then
           if [ -n "$DEPTH" ]; then
-            echo 'git remote set-branches origin '*' - before'
             git_retry git remote set-branches origin '*'
-            echo 'git remote set-branches origin '*' - after'
-            echo 'git fetch --depth=$DEPTH'
             git_retry git fetch --depth=$DEPTH
-            echo 'git fetch --depth=$DEPTH - after'
           fi
         git_checkout
       fi
@@ -215,12 +211,8 @@ else
   echo "cloned successfully into $CLONE_DIR"
   if [ -n "$REVISION" ]; then
       if [ -n "$DEPTH" ]; then
-        echo 'git remote set-branches origin '*' - before'
-        git_retry git remote set-branches origin '*'
-        echo 'git remote set-branches origin '*' - after'
-        echo "git fetch --depth=$DEPTH"
-        git_retry git fetch --depth=$DEPTH
-        echo 'git fetch --depth=$DEPTH - after'
+        git remote set-branches origin '*'
+        git fetch --depth=$DEPTH
       fi
     git_checkout
   fi
