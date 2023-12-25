c58114444cd0c2a93d2612fed654a8d74a84487f
Kim <kim.aharfi@codefresh.io>
2023-12-25 17:00:40 +0200

    fix fetch with revision without change clone logic

--- start.sh
+++ start.sh
@@ -224,7 +224,7 @@ if [ -d "$CLONE_DIR" ]; then
       if [ -n "$REVISION" ]; then
           if [ -n "$DEPTH" ]; then
             git_retry git remote set-branches origin "*"
-            git_retry git fetch --depth=$DEPTH
+            git_retry git origin $REVISION fetch --depth=$DEPTH
           fi
         git_checkout
       fi
